a32d7019a8b3de0d6e8139d77243a3570c00d086
nitinchalopadho <nitin@chalopadho.com>
2018-08-20 15:54:11 +0530

    initial commit

--- cypress/integration/tests/video_spec.js
+++ cypress/integration/tests/video_spec.js
@@ -1,3 +1,8 @@
 describe('it should play the video', function() {
-    
+    beforeEach(
+        cy.visit('https://nitingupta220.github.io/cypress-video-testing/')
+    )
+    it('should play the video', function() {
+        cy.get('video').click();
+    })
 })
